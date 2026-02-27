b6919d09eb551346a6c57d75d5f31bf475377413
anthonyapitchou <anthoapicella@hotmail.fr>
2026-02-27 15:30:48 +0100

    Initial project structure

---
+++ js/settings.js
@@ -0,0 +1,14 @@
+const portfolioData = {
+    name: "Anthony",
+    role: "Front-end Developer",
+    socials: {
+        github: "https://github.com",
+        linkedin: "https://linkedin.com..."
+    },
+    projects: [
+        { title: "RainyDays", desc: "E-commerce for jackets", link: "#" },
+        { title: "Portfolio", desc: "This assignment", link: "#" }
+    ]
+};
+
+export default portfolioData;
